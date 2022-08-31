721bdc30c0fc61ed4803b1d4d9dbae09b8c75a2d
jokerwrld999 <ifalaleev49@gmail.com>
2022-08-31 23:39:30 +0300

    DatedCopy Upgradedd

--- Variables/3_DatedCopyOfFile.sh
+++ Variables/3_DatedCopyOfFile.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# >>>> Variabels
+# >>>> Global Variables
 # *** Filename Argument
 FILE=$1
 # *** Date In Format Year-Month-Day
 DATE=$(date +%Y-%m-%d)
 # *** Delimiter Type
 DELIMITER="_"
-# *** New Filename
-FILENAME=$DATE$DELIMITER$FILE
+
+# >>> Set Filename With Date First
+BACKUPNAME=$DATE$DELIMITER$FILE
+
+# >>>> Set Another Filename Format
+# *** Get Name Of The File
+FILENAME=$FILE | cut -d\. -f1
+# *** Get Extention Of The File
+EXTENTION=$FILE | sed -E 's|(.[^.]*){1}||'
+
+# *** Set Full Name Of The File
+NAMEOFBACKUP=$FILENAME$DELIMITER$EXTENTION
+
 
 # >>>> Backup With New Filename
-cp $1 $FILENAME
+cp $1 $BACKUPNAME
+cp $1 $NAMEOFBACKUP
+
