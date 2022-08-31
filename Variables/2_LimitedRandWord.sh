ed30bce0cce2f263d48f161b9aa3492ba07c5e02
jokerwrld999 <ifalaleev49@gmail.com>
2022-08-31 21:37:40 +0300

    Created another script for words with limited number of characters

---
+++ Variables/2_LimitedRandWord.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# >>>> Variables
+# *** The Char Length Argument
+CHARLENGTH=$1
+# *** Set File Directory
+DIR=/usr/share/dict/words
+# *** Get Number Of Records
+RECORDS=$(wc -l < $DIR)
+# *** Generating Random Number In Range Of Records
+RandInRage=$(($RANDOM%$RECORDS+1))
+
+# >>>> Get Random Word From File
+LIMITWORD=$(grep -# "^.{$CHARLENGTH}$" $DIR | sed -n "$RandInRage p")
+echo "Your random word is $LIMITWORD from $DIR file"
