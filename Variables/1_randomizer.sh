ad9de0e3cc980cd9242532bcb9b9987400d6867d
jokerwrld999 <ifalaleev49@gmail.com>
2022-08-31 15:09:22 +0300

    Randomizer final version

--- Variables/1_randomizer.sh
+++ Variables/1_randomizer.sh
@@ -9,5 +9,5 @@ RECORDS=$(wc -l < $DIR)
 RandInRage=$(($RANDOM%$RECORDS+1))
 
 # >>>> Get Random Word From File
-WORD=$((cat $DIR | sed -n "$RandInRage p"))
+WORD=$(cat $DIR | sed -n "$RandInRage p")
 echo "Your random word is $WORD from $DIR file"
