bb006682ea3062ee00e1c75ca4a51abf3ee24fc5
jokerwrld999 <ifalaleev49@gmail.com>
2022-08-31 22:42:26 +0300

    DatedCopy polished

--- Variables/3_DatedCopyOfFile.sh
+++ Variables/3_DatedCopyOfFile.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# >>>> Variabels
+# *** Filename Argument
 FILE=$1
+# *** Date In Format Year-Month-Day
 DATE=$(date +%Y-%m-%d)
+# *** Delimiter Type
+DELIMITER="_"
+# *** New Filename
+FILENAME=$DATE$DELIMITER$FILE
 
-FILENAME="$DATE_$FILE"
+# >>>> Backup With New Filename
 cp $1 $FILENAME
