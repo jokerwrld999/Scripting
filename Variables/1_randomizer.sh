fe0aa6e5e608491a275d32af20d4b0526e93c176
jokerwrld999 <ifalaleev49@gmail.com>
2022-08-31 15:06:29 +0300

    Randomizer final checkl

---
+++ Variables/1_randomizer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# >>>> Variables
+# *** Set File Directory
+DIR=/usr/share/dict/words
+# *** Get Number Of Records
+RECORDS=$(wc -l < $DIR)
+# *** Generating Random Number In Range Of Records
+RandInRage=$(($RANDOM%$RECORDS+1))
+
+# >>>> Get Random Word From File
+WORD=$((cat $DIR | sed -n "$RandInRage p"))
+echo "Your random word is $WORD from $DIR file"
--- Variables/randomizer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# >>>> Variables
-DIR=/usr/share/dict/words
-RECORDS=$(wc -l < $DIR)
-RandInRage=$(($RANDOM%$RECORDS+1))
-
-cat $DIR | sed -n "$RandInRage p"
