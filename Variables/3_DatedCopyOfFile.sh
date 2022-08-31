4b07603000e606b5e912712c8dc43188339eb9d6
jokerwrld999 <ifalaleev49@gmail.com>
2022-08-31 23:46:52 +0300

    DatedCopy Fix

--- Variables/3_DatedCopyOfFile.sh
+++ Variables/3_DatedCopyOfFile.sh
@@ -13,9 +13,9 @@ BACKUPNAME=$DATE$DELIMITER$FILE
 
 # >>>> Set Another Filename Format
 # *** Get Name Of The File
-FILENAME=$($FILE | cut -d\. -f1)
+FILENAME=$(echo "$FILE" | cut -d\. -f1)
 # *** Get Extention Of The File
-EXTENTION=$($FILE | sed -E 's|(.[^.]*){1}||')
+EXTENTION=$(echo "$FILE" | sed -E 's|(.[^.]*){1}||')
 
 # *** Set Full Name Of The File
 NAMEOFBACKUP=$FILENAME$DELIMITER$DATE$EXTENTION
