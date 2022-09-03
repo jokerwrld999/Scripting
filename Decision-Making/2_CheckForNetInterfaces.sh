1fadffbf30d5ff0bf4fef41cff2a369315e84b8b
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-03 11:43:17 +0300

    Network Interfaces Search Nested

--- Decision-Making/2_CheckForNetInterfaces.sh
+++ Decision-Making/2_CheckForNetInterfaces.sh
@@ -3,12 +3,15 @@
 # Find The Number Of Network Interfaces
 INTERFACES=$(ip address | grep -v LOOPBACK | grep -ic mtu)
 
-if [ $INTERFACES -eq 1 ]
+if [ $INTERFACES -gt 1 ]
 then
-    echo "1 Active Network Interface Found"
-elif [ $INTERFACES -gt 1 ]
-then
-    echo "Found Multiple Active Network Interfaces"
+    if [ $INTERFACES -eq 1 ]
+    then
+        echo "1 Active Network Interface Found"
+    elif [ $INTERFACES -gt 1 ]
+    then
+        echo "Found Multiple Active Network Interfaces"
+    fi
 else
     echo "No Active Network Interfaces Found."
 fi
