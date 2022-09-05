2d18eaf650526a859e2f40b15aa062d2004145e6
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-05 14:07:47 +0300

    Logic Rewrite

--- Decision-Making/4_ArchiveToggle.sh
+++ Decision-Making/4_ArchiveToggle.sh
@@ -5,7 +5,7 @@ BACKUPFILE=backup.tar.bz2
 ISARCHIVE= echo $1 | sed -n 's/.*\(.t[ab]\)/\1/p' 
 
 
-if [[ -n $1 ]] && [[ -e $1 ]] || [[ -d $1 ]]
+if [[ -n $1 ]] && [[ $ISARCHIVE = ".tar.bzip2" ]] || [[ $ISARCHIVE = ".tar.bz2" ]] || [[ $ISARCHIVE = ".tb2" ]] 
 then
     echo "Okay, let's archive $1"
     tar -cjvf $BACKUPFILE $1
@@ -15,7 +15,7 @@ then
     else
         echo "There was an error while archiving $1"
     fi
-elif [[ -n $1 ]] && [[ $ISARCHIVE = ".tar.bzip2" ]] || [[ $ISARCHIVE = ".tar.bz2" ]] || [[ $ISARCHIVE = ".tb2" ]]
+elif [[ -n $1 ]] && [[ -e $1 ]] || [[ -d $1 ]] 
 then
     echo "Oh, there is an archive let's unarchive it"
     tar -xjvf $BACKUPFILE
