fabddf7e8a26c382b0ae301da2878e7b0644898f
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-05 15:25:45 +0300

    Statements check

--- Decision-Making/4_ArchiveToggle.sh
+++ Decision-Making/4_ArchiveToggle.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 # >>> Variables
-BACKUPFILE=backup.tar.bz2
-ISARCHIVE= echo $1 | sed -n 's/.*\(.t[ab]\)/\1/p' 
+BACKUPFILE=backup.tbz
+ISARCHIVE=$(echo "test.tar.bz2" | sed -n 's/.*\(.t[ab]\)/\1/p') 
 
 
-if [[ -n $1 ]] && [[ $ISARCHIVE = ".tar.bzip2" ]] || [[ $ISARCHIVE = ".tar.bz2" ]] || [[ $ISARCHIVE = ".tb2" ]] 
+if [[ -n $1 ]] && [[ "$ISARCHIVE" == ".tbz" ]] || [[ "$ISARCHIVE" == ".tar.bz2" ]] || [[ "$ISARCHIVE" == ".tb2" ]]; 
 then
-   echo "Oh, there is an archive let's unarchive it"
-    tar -xjvf $BACKUPFILE
-    if [ $? -eq 0 ]
+   echo "Oh, there is an archive, let's unarchive it"
+    tar -xjvf $1
+    if [ $? = 0 ]
     then
         echo "Your $BACKUPFILE unarchived succsessfully"
     else
         echo "There was an error while unarchiving $BACKUPFILE"
     fi
-elif [[ -n $1 ]] && [[ -e $1 ]] || [[ -d $1 ]] 
-then
-    echo "Okay, let's archive $1"
-    tar -cjvf $BACKUPFILE $1
-    if [ $? -eq 0 ]
+else 
+    if [[ -n $1 ]] && [[ -e $1 ]] || [[ -d $1 ]] 
     then
-        echo "Your $1 archived succsessfully"
-    else
-        echo "There was an error while archiving $1"
+        echo "Okay, let's archive $1"
+        tar -cjvf $BACKUPFILE $1
+        if [ $? = 0 ]
+        then
+            echo "Your $1 archived succsessfully"
+        else
+            echo "There was an error while archiving $1"
+        fi
     fi 
 fi
