a4a852d789092a8a8ad6c1dc1229f3099a1e9902
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-05 14:07:47 +0300

    Logic Rewrite

--- Decision-Making/4_ArchiveToggle.sh
+++ Decision-Making/4_ArchiveToggle.sh
@@ -5,7 +5,17 @@ BACKUPFILE=backup.tar.bz2
 ISARCHIVE= echo $1 | sed -n 's/.*\(.t[ab]\)/\1/p' 
 
 
-if [[ -n $1 ]] && [[ -e $1 ]] || [[ -d $1 ]]
+if [[ -n $1 ]] && [[ $ISARCHIVE = ".tar.bzip2" ]] || [[ $ISARCHIVE = ".tar.bz2" ]] || [[ $ISARCHIVE = ".tb2" ]] 
+then
+   echo "Oh, there is an archive let's unarchive it"
+    tar -xjvf $BACKUPFILE
+    if [ $? -eq 0 ]
+    then
+        echo "Your $BACKUPFILE unarchived succsessfully"
+    else
+        echo "There was an error while unarchiving $BACKUPFILE"
+    fi
+elif [[ -n $1 ]] && [[ -e $1 ]] || [[ -d $1 ]] 
 then
     echo "Okay, let's archive $1"
     tar -cjvf $BACKUPFILE $1
@@ -14,15 +24,5 @@ then
         echo "Your $1 archived succsessfully"
     else
         echo "There was an error while archiving $1"
-    fi
-elif [[ -n $1 ]] && [[ $ISARCHIVE = ".tar.bzip2" ]] || [[ $ISARCHIVE = ".tar.bz2" ]] || [[ $ISARCHIVE = ".tb2" ]]
-then
-    echo "Oh, there is an archive let's unarchive it"
-    tar -xjvf $BACKUPFILE
-    if [ $? -eq 0 ]
-    then
-        echo "Your $BACKUPFILE unarchived succsessfully"
-    else
-        echo "There was an error while unarchiving $BACKUPFILE"
-    fi
+    fi 
 fi
