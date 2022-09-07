5b56c3bd6d661604816eeafb9e4cf407c627d3c1
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-07 11:37:39 +0300

    Range of numbers

--- BashScripting-Tutorial/2_UserInput/1_PrintALine.sh
+++ BashScripting-Tutorial/2_UserInput/1_PrintALine.sh
@@ -2,30 +2,19 @@
 
 # >>>> Number Of Line
 echo "Hey, what line do you want to print?"
+echo ======================================
+echo
 read -p "Line Number: " LINENUM
+echo ==========================
+echo
 
 # >>>> Number to echo
-case $LINENUM in 
-
-    1)
-        FORMATNUM="$LINENUM"st
-        ;;
-
-    2)
-        FORMATNUM="$LINENUM"nd
-        ;;
-
-    3)
-        FORMATNUM="$LINENUM"rd
-        ;;
-
-    [4-20])
-        FORMATNUM="$LINENUM"th
-        ;;
-
-    *)
-        FORMATNUM="$LINENUM"
-        ;;
+case $LINENUM in
+    ( 1 | [2-9]1 ) FORMATNUM="$LINENUM"st ;;
+    ( 2 | [2-9]2 ) FORMATNUM="$LINENUM"nd ;;
+    ( 3 | [2-9]3 ) FORMATNUM="$LINENUM"rd ;;
+    ( [4-9] | 1[0-9] ) FORMATNUM="$LINENUM"th ;;
+    ( * ) FORMATNUM="$LINENUM" ;;
 esac
 
 
