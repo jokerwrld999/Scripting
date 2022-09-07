493f4164e7b06ff4a60d7d95531e91b814a683ba
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-07 18:17:20 +0300

    Input Validation FINISHED

--- BashScripting-Tutorial/2_UserInput/2_FilterOfCommand.sh
+++ BashScripting-Tutorial/2_UserInput/2_FilterOfCommand.sh
@@ -11,22 +11,24 @@ echo "Please, type Y or N for what you want"
 echo ====================
 echo
 
-FIELDS="PERMS NUMLINKS OWNER"
+FIELDS="PERMS NUMLINKS OWNER GROUP SIZE DATE FILENAME"
 for FIELD in $FIELDS
 do
     while :
     do
         case $FIELD in
-        PERMS ) MSG="Permissions - " ;;
-        NUMLINKS ) MSG="Number of links - " ;;
-        OWNER ) MSG="File Owner - " ;;
-        GROUP ) MSG="File Group - " ;;
-        SIZE ) MSG="File Size - " ;;
-        DATE ) MSG="Date and Time - " ;;
-        FILENAME ) MSG="Filename - " ;;
-        * ) MSG="Incorrect Data"
-    esac
+        ( PERMS ) MSG="Permissions - " ;;
+        ( NUMLINKS ) MSG="Number of links - " ;;
+        ( OWNER ) MSG="File Owner - " ;;
+        ( GROUP ) MSG="File Group - " ;;
+        ( SIZE ) MSG="File Size - " ;;
+        ( DATE ) MSG="Date and Time - " ;;
+        ( FILENAME ) MSG="Filename - " ;;
+        ( * ) MSG="Incorrect Data" ;;
+        esac
+
         read -p "$MSG" TMP
+
         if [[ $TMP =  "Y" ]] || [[ $TMP = N ]]
         then
             export $FIELD=$TMP
