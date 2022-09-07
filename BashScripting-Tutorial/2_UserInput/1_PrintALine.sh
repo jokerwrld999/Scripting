6fe3517211c37d666b29e8a29deff2c152cd6453
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-07 11:23:08 +0300

    Ordinal numbers output

--- BashScripting-Tutorial/2_UserInput/1_PrintALine.sh
+++ BashScripting-Tutorial/2_UserInput/1_PrintALine.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-echo "Here is a third line of the $1:"
+# >>>> Number Of Line
+echo "Hey, what line do you want to print?"
+read LINENUM
+
+# >>>> Number to echo
+case $LINENUM in 
+
+    1)
+        FORMATNUM="$LINENUM"+"st"
+        ;;
+
+    2)
+        FORMATNUM="$LINENUM"+"nd"
+        ;;
+
+    3)
+        FORMATNUM="$LINENUM"+"rd"
+        ;;
+
+    {4..20})
+        FORMATNUM="$LINENUM"+"th"
+        ;;
+
+    *)
+        FORMATNUM="$LINENUM"
+        ;;
+esac
+
+
+echo "Here is the $FORMATNUM line of your file:"
 echo ===============================
 echo 
 
-cat /dev/stdin | sed -n "3 p"
+cat /dev/stdin | sed -n "$LINENUM p"
