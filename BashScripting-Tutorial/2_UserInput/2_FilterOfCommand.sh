ad2e766f266f4a0e222b7f7ccbe26a1f493c4843
jokerwrld999 <ifalaleev49@gmail.com>
2022-09-07 14:07:18 +0300

    Input Validation

--- BashScripting-Tutorial/2_UserInput/2_FilterOfCommand.sh
+++ BashScripting-Tutorial/2_UserInput/2_FilterOfCommand.sh
@@ -11,10 +11,31 @@ echo "Please, type Y or N for what you want"
 echo ====================
 echo
 
-read -p "Permissions - " PRERMS
-read -p "Number of hard links to the file - " NUMLINKS
-read -p "File Owner - " OWNER
-read -p "File Group - " GROUP
-read -p "File Size - " SIZE
-read -p "Date and Time - " DATE
-read -p "Filename - " FILENAME 
+FIELDS="PERMS NUMLINKS OWNER"
+for FIELD in $FIELDS
+do
+    while :
+    do
+        case $FIELD in
+        PERMS ) MSG="Permissions - " ;;
+        NUMLINKS ) MSG="Number of links - " ;;
+        OWNER ) MSG="File Owner - " ;;
+        GROUP ) MSG="File Group - " ;;
+        SIZE ) MSG="File Size - " ;;
+        DATE ) MSG="Date and Time - " ;;
+        FILENAME ) MSG="Filename - " ;;
+        * ) MSG="Incorrect Data"
+    esac
+        read -p "$MSG" TMP
+        if [[ $TMP =  "Y" ]] || [[ $TMP = N ]]
+        then
+            export $FIELD=$TMP
+            echo "Great"
+            break
+        else 
+            echo "Are you retarded or what?"
+            echo "Commone you stupid ass, just type Y or N" 
+        fi
+    done   
+done
+echo $FIELDS
