a8e04eea4e1378254b47c1f4923e8aae91199f83
jokerwrld999 <ifalaleev49@gmail.com>
2022-10-05 17:52:16 +0300

    arithmetic script

---
+++ BashScripting-Tutorial/3_arithmetic/0_expansionExample.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Basic arithmetic using double parentheses
+a=$(( 4 + 5 ))
+echo $a # 9
+a=$((3+5))
+echo $a # 8
+b=$(( a + 3 ))
+echo $b # 11
+b=$(( $a + 4 ))
+echo $b # 12
+(( b++ ))
+echo $b # 13
+(( b += 3 ))
+echo $b # 16
+a=$(( 4 * 5 ))
+echo $a # 20
+
+export counter=2
+
+while [ $counter < 5 ]
+do
+    (($counter++))
+    echo $counter
+done
