8585cbb3431d3e8191530388db45bb9ed7a585a7
jokerwrld999 <ifalaleev49@gmail.com>
2022-10-09 22:40:58 +0300

    Multilyer script

--- BashScripting-Tutorial/4_Arithmetic/1_Multiplyer.sh
+++ BashScripting-Tutorial/4_Arithmetic/1_Multiplyer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Basic arithmetic using double parentheses
 #let
-letMethod=$(let "$1 * $2")
+let "letMethod = $1 * $2"
 echo "Multiplyed by LET: $letMethod"
 
 #expr
-exprMethod=$(expr "$1 * $2")
+exprMethod=$( expr $1 \* $2 )
 echo "Multiplyed by EXPR: $exprMethod"
 
 #double parentheses
