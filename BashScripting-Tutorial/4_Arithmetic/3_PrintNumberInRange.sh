62058a396c83c8f7b80b0e90298866fcd7110e99
jokerwrld999 <ifalaleev49@gmail.com>
2022-11-18 22:57:20 +0200

    Done With Arithmetic Section

--- BashScripting-Tutorial/4_Arithmetic/3_PrintNumberInRange.sh
+++ BashScripting-Tutorial/4_Arithmetic/3_PrintNumberInRange.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-range=$(( $RANDOM%100 ))
-echo $range
+echo -ne "
+========================================================================
+Hi! This script can give you any "RANDOM" number in your given range
+========================================================================
+"
+read -p "Please, enter the lower limit: " lower_limit
+read -p "Please, enter the upper limit: " upper_limit
+
+random_numer=$(shuf -i $lower_limit-$upper_limit -n 1)
+echo "Here your random namber is - " $random_numer
